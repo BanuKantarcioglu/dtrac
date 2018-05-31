26e35f9a18da65755356e6b753643719b9086d37
Banu Kantarcioglu <Banu.Kantarcioglu@gmail.com>
2018-05-31 21:13:04 +0300

    prepare seed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+#  http_basic_authenticate_with name: "user", password: "user"
 
   def hello
     render html: "hello, world!"
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+
+100.times do |n|
+  name  = Faker::Name.name
+  pno = Faker::Number.number(5)
+  jobdescription = Faker::Job.title
+  status = Faker::Boolean.boolean(0.8)
+  p = Personnel.create!(name:  name,
+               pno:   pno ,
+               jobdescription: jobdescription,
+               status: status)
+  numofdocuments = Faker::Number.between(1,4)
+  documenttype = ["","Work Permit","Visa","Document XY36","Some other document"]
+  numofdocuments.times do |k|
+    dname = documenttype[Faker::Number.between(1,4)]
+    startdate = Faker::Date.between(1.years.ago,Date.today)
+    enddate = Faker::Date.between(startdate,startdate + 365.days)
+    p.documents.create!(name: dname,
+      startdate: startdate,
+      enddate: enddate,
+      status: 1
+    )
+  end
+
+end
